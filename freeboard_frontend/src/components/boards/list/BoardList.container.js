ddaf5b93ec969203e27dd57eacd6a98bde73a071
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2023-10-04 10:49:53 +0900

    [포트폴리오] 게시글 목록 화면, 게시글 상세 삭제하기 기능 구현

---
+++ freeboard_frontend/src/components/boards/list/BoardList.container.js
@@ -0,0 +1,25 @@
+import { useQuery } from "@apollo/client";
+import { FETCH_BOARDS } from "./BoardList.queries";
+import BoardListUI from "./BoardList.presenter";
+import { useRouter } from "next/router";
+
+export default function BoardList() {
+  const { data } = useQuery(FETCH_BOARDS, {
+    variables: {
+      page: 133,
+    },
+  });
+
+  const router = useRouter();
+
+  const onClickMoteToBoardDetail = (e) => {
+    router.push(`/boards/${e.target.id}`);
+  };
+
+  const onClickMoteToBoardNew = () => {
+    router.push(`/boards/new`);
+  };
+
+  // 날짜 형식 변환 방법 모르겠음!
+  return <BoardListUI data={data} onClickMoteToBoardDetail={onClickMoteToBoardDetail} onClickMoteToBoardNew={onClickMoteToBoardNew} />;
+}
---
+++ freeboard_frontend/src/components/boards/list/BoardList.presenter.js
@@ -0,0 +1,39 @@
+import * as S from "./BoardList.styles";
+import { getDate } from "../../../../src/commons/libraries/utils";
+
+export default function BoardListUI(props) {
+  return (
+    <S.Wrapper>
+      <S.TableTop>
+        <S.Row>
+          <S.ColumnHeaderBasic>ID</S.ColumnHeaderBasic>
+          <S.ColumnHeaderTitle>제목</S.ColumnHeaderTitle>
+          <S.ColumnHeaderBasic>작성자</S.ColumnHeaderBasic>
+          <S.ColumnHeaderBasic>날짜</S.ColumnHeaderBasic>
+        </S.Row>
+      </S.TableTop>
+      <S.TableBottom>
+        {props.data?.fetchBoards.map((el) => (
+          <S.Row key={el._id}>
+            {/* ID */}
+            <S.ColumnBasic>{String(el._id).slice(-4).toUpperCase()}</S.ColumnBasic>
+            {/* 제목 */}
+            <S.ColumnTitle id={el._id} onClick={props.onClickMoteToBoardDetail}>
+              <a href="">{el.title}</a>
+            </S.ColumnTitle>
+            {/* 작성자 */}
+            <S.ColumnBasic>{el.writer}</S.ColumnBasic>
+            {/* 날짜 */}
+            <S.ColumnBasic>{getDate(el.createdAt)}</S.ColumnBasic>
+          </S.Row>
+        ))}
+      </S.TableBottom>
+      <S.Footer>
+        <S.Button onClick={props.onClickMoteToBoardNew}>
+          <S.PencilIcon src="/images/board/list/write.png" />
+          게시물 등록하기
+        </S.Button>
+      </S.Footer>
+    </S.Wrapper>
+  );
+}
