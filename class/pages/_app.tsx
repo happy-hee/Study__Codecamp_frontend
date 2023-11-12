fbf1f9e0d543c6d3b125f434f167cafb3816fe95
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2023-11-12 05:25:09 +0000

    [학습] 14-컴포넌트 합성

--- class/pages/_app.tsx
+++ class/pages/_app.tsx
@@ -1,24 +1,18 @@
 // GraphQL 세팅
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { AppProps } from "next/app";
 import Layout from "../src/components/commons/layout";
+import ApolloSetting from "../src/components/commons/apollo";
 
 export default function App({ Component }: AppProps) {
-  // GraphQL 세팅
-  const client = new ApolloClient({
-    uri: "http://backend-example.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(), // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기 => 나중에 더 자세히 알아볼 예정
-  });
-
   return (
     <div>
       <div>====================== 여기는 _app.js 컴포넌트 시작부분 입니다. ======================</div>
       {/* // 아래 컴포넌트에서 GraphQL을 사용할 수 있도록 감싸줌 */}
-      <ApolloProvider client={client}>
+      <ApolloSetting>
         <Layout>
           <Component />
         </Layout>
-      </ApolloProvider>
+      </ApolloSetting>
       <div>====================== 여기는 _app.js 컴포넌트 끝부분 입니다. ======================</div>
     </div>
   );
