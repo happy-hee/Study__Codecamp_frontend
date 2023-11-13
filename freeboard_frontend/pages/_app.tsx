88bbd7377f8ff981617162faf8bd26bf288e9255
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2023-11-13 05:57:08 +0000

    [포트폴리오] Apollo 세팅 분리

--- freeboard_frontend/pages/_app.tsx
+++ freeboard_frontend/pages/_app.tsx
@@ -1,27 +1,22 @@
-// import "../styles/globals.css";
 // GraphQL 세팅
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { AppProps } from "next/app";
 import Layout from "../src/components/commons/layout";
 import { Global } from "@emotion/react";
 import { globalStyles } from "../src/commons/styles/globalStyles";
+import ApolloSetting from "../src/components/commons/apollo";
 
 export default function App({ Component }: AppProps) {
-  // GraphQL 세팅
-  const client = new ApolloClient({
-    uri: "http://backend-practice.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(), //컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기 => 나중에 더 자세히 알아볼 예정
-  });
-
   return (
-    // 아래 컴포넌트에서 GraphQL을 사용할 수 있도록 감싸줌
-    <ApolloProvider client={client}>
-      <Global styles={globalStyles} />
+    <div>
+      // 아래 컴포넌트에서 GraphQL을 사용할 수 있도록 감싸줌
+      <ApolloSetting>
       <>
+        <Global styles={globalStyles} />
         <Layout>
           <Component />
         </Layout>
       </>
-    </ApolloProvider>
+      </ApolloSetting>
+    </div>
   );
 }
