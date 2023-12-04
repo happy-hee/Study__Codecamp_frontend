64c961f0f13d1b6a24a74441f07b9fb8f16a3431
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-12-04 23:21:30 +0900

    [학습] 18-데코레이터 이해

---
+++ class_backend/decorator-example.ts
@@ -0,0 +1,11 @@
+function bbb(qqqqq: any) {
+  console.log("========================");
+  console.log(qqqqq); // class Board
+  console.log("========================");
+}
+
+@bbb // <┐  clss Board가 bbb 라는 함수에 인자로 들어감
+class Board {
+  // number: number
+  // writer: string
+}
